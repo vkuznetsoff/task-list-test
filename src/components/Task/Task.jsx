acc3c23cb3e11d3013b5162009499f4e0ded24eb
Slavich <ss_slavich@mail.ru>
2022-06-23 17:38:57 +0300

    Create table of data with grid layout and add paginator

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -9,23 +9,23 @@ const CreateName = (obj) => {
 const Task = ({ task }) => {
   return (
     <div className="content">
-      <div className="content__row">
-        <div className="content__item">
-          <div className="content__item__head">№{task.id}</div>
-          <div className="content__item__text">{task.created_date}</div>
+      <div className="content__grid">
+        <div className="grid__item item_title">
+          <div className="grid__item__head">№{task.id}</div>
+          <div className="grid__item__text">{task.created_date}</div>
         </div>
-        <div className="content__item">
-          <div className="content__item__head">{task.order_type.name}</div>
-          <div className="content__item__text">
+        <div className="grid__item item_task ">
+          <div className="grid__item__head">{task.order_type.name}</div>
+          <div className="grid__item__text">
             {CreateName(task.created_user)}
           </div>
         </div>
-        <div className="content__item">
-          <div className="content__item__head">{task.account.name}</div>
-          <div className="content__item__text">{task.terminal.name}</div>
+        <div className="grid__item item_info ">
+          <div className="grid__item__head nowrap">{task.account.name}</div>
+          <div className="grid__item__text nowrap">{task.terminal.name}</div>
         </div>
-        <div className="content__item">
-          <div className="content__item__head">{task.status}</div>
+        <div className="grid__item item_status">
+          <div className="grid__item__head">{task.status}</div>
         </div>
       </div>
     </div>
